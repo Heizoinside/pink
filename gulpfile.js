fdac9f1981213556f7f06563b64f97ff124f44db
Kotov Filipp <heizoinside@gmail.com>
2019-01-21 21:58:32 +0300

    настроил автоматизацию

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,34 @@
-var less = require('gulp-less');
-var path = require('path');
+"use strict";
 
-gulp.task('less', function () {
-  gulp.src("less/style.less")
-  .pipe(less())
-  .pipe(gulp.dest("css"));
+var gulp = require("gulp");
+var less = require("gulp-less");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+
+gulp.task("css", function () {
+  return gulp.src("less/style.less")
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("css"))
+    .pipe(server.stream());
 });
+
+gulp.task("server", function () {
+  server.init({
+    server: ".",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("less/**/*.less", gulp.series("css"));
+  gulp.watch("source/*.html").on("change", server.reload);
+});
+
+gulp.task("start", gulp.series("css", "server"));
